2c87c1ad586e7e7380f904036ad6b49d260dba9d
code-death <poorvanshkavta938@gmial.com>
2022-06-30 16:04:37 +0530

    Made the content box and star back

--- src/Components/Main/Rating.js
+++ src/Components/Main/Rating.js
@@ -1,10 +1,13 @@
 import './Rating.css'
 import Card from '../Card/Card'
+import Star from '../UI/Star'
 
 const Rating = props => {
   return (
     <div className='rating'>
-      <Card />
+      <Card>
+        <Star />
+      </Card>
     </div>
   )
 }
---
+++ src/Components/UI/Star.js
@@ -0,0 +1,7 @@
+import './Star.css'
+
+const Star = props => {
+  return <div className='star'></div>
+}
+
+export default Star
